ebacdf99527dcb7fabf417554669b5821e676fb5
Efraim Rodrigues <efraimnaassom@gmail.com>
2019-06-26 13:49:55 -0300

    - Adds contact-me section; - Includes blog posts in the main file; - Transfer personal information to the navbar.

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ sudo rm -rf _site
 sudo rm -rf _cache
 
 yui-compressor css/resume.css > css/resume.min.css
+yui-compressor js/resume.js > js/resume.min.js
+stack setup
 
 sudo stack build --allow-different-user
 sudo stack exec blog build --allow-different-user
